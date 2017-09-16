46d3a925a3cc4848fedc45e9f4bf9966aa263df5
Nicolas Seydoux <nseydoux@laas.fr>
2017-09-17 00:49:41 +0200

    Added output test

---
+++ ada-project/hello_world_correction.adb
@@ -0,0 +1,5 @@
+with Text_IO;
+procedure Hello_World_Correction is
+    begin
+        Text_IO.Put_line("Hello World!");
+    end Hello_World_Correction;
--- ada-project/start.sh
+++ ada-project/start.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 
 gnatmake hello_world.adb
-./hello_world
+gnatmake hello_world_correction.adb 2> /dev/null
+./hello_world | tee log.out
+./hello_world_correction > correction.out
+if [ -z `diff log.out correction.out`] then
+	echo "Correct !"
+else
+	echo "Incorrect\n"
+	echo "Expected output : "
+	cat correction.out
+	echo "\n got : "
+	cat log.out
