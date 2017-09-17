b7cfddad3d2ef62b37c22da72c6e3a16aaac51c7
Nicolas Seydoux <nseydoux@laas.fr>
2017-09-17 17:21:50 +0200

    First exercise code

--- projects/01_warm-up-project/test_io.adb
+++ projects/01_warm-up-project/test_io.adb
@@ -1,9 +1,17 @@
 with Ada.Text_IO;
- 
-procedure Main is
+with Ada.Strings.Fixed;
+
+procedure test_io is
   Str  : String (1 .. 80);
+  Number_lines : Integer;
+  Current_value : Integer;
   Last : Natural;
 begin
   Ada.Text_IO.Get_Line (Str, Last);
-  Ada.Text_IO.Put_Line (Str (1 .. Last));
-end;
+  Number_lines := Integer'Value(Str(1..Last));
+  for i in 1..Number_lines loop
+  	Ada.Text_IO.Get_Line (Str, Last);
+  	Current_value := Integer'Value(Str(1..Last));
+  	Ada.Text_IO.Put_Line(Integer'Image(Current_value*2));
+  end loop;
+end test_io;
